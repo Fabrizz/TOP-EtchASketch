b9e17aacd6c95db3a6dd289b0082afc07ef8073e
Fabrizz <65259076+Fabrizz@users.noreply.github.com>
2022-01-18 22:19:11 -0300

    Device test | No JS

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-/*
-eas-interact-color
-eas-interact-selected
-eas-interact-rainbow
-eas-interact-grayscale
-eas-interact-reset
-eas-interact-size
-*/
+let modes = [selected, rainbow, grayscale];
+
+let config = {
+    currentColor: "",
+    gridSize: 16,
+    colorMode: 0,
+}
+
+const interactListColor =  document.getElementById("eas-interact-selected");
+const interactListRainbow =  document.getElementById("eas-interact-rainbow");
+const interactListGray =  document.getElementById("eas-interact-grayscale");
+
+const interactColorPicker =  document.getElementById("eas-interact-color");
+const interactBtnReset =  document.getElementById("eas-interact-reset");
+const interactSliderSize =  document.getElementById("eas-interact-size");
+
+const mpGridSize = document.getElementById("eas-mp-size");
+const mpGrid = document.getElementById("eas-mp-grid");
